00b27b68aad798966f19412e51976568220dc970
Carlos Tiguaque <c.tiguaque@uniandes.edu.co>
2025-04-02 10:08:06 -0500

    Se ajusta pruebas unitarias

--- karma.conf.js
+++ karma.conf.js
@@ -1,44 +1,44 @@
 module.exports = function (config) {
     config.set({
-      basePath: '',
-      frameworks: ['jasmine', '@angular-devkit/build-angular'],
-      plugins: [
-        require('karma-jasmine'),
-        require('karma-chrome-launcher'),
-        require('karma-coverage'),
-        require('@angular-devkit/build-angular/plugins/karma')
-      ],
-      client: {
-        jasmine: {},
-        clearContext: false
-      },
-      coverageReporter: {
-        dir: require('path').join(__dirname, './coverage'),
-        subdir: '.',
-        reporters: [
-          { type: 'html' },
-          { type: 'text-summary' }
-        ]
-      },
-      reporters: ['progress', 'coverage'],
-      port: 9876,
-      colors: true,
-      logLevel: config.LOG_INFO,
-      autoWatch: false,
-      customLaunchers: {
-        ChromeHeadlessCI: {
-          base: 'ChromeHeadless',
-          flags: [
-            '--no-sandbox',
-            '--headless',
-            '--disable-gpu',
-            '--disable-dev-shm-usage',
-            '--remote-debugging-port=9222'
-          ]
-        }
-      },
-      browsers: ['ChromeHeadlessCI'],
-      singleRun: true,
-      restartOnFileChange: false
+        basePath: '',
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-coverage'),
+            require('@angular-devkit/build-angular/plugins/karma')
+        ],
+        client: {
+            jasmine: {},
+            clearContext: false
+        },
+        coverageReporter: {
+            dir: require('path').join(__dirname, './coverage'),
+            subdir: '.',
+            reporters: [
+                { type: 'html' },
+                { type: 'text-summary' }
+            ]
+        },
+        reporters: ['progress', 'coverage'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: false,
+        customLaunchers: {
+            ChromeHeadlessCI: {
+                base: 'ChromeHeadless',
+                flags: [
+                    '--no-sandbox',
+                    '--disable-gpu',
+                    '--disable-dev-shm-usage',
+                    '--headless',
+                    '--remote-debugging-port=9222'
+                ]
+            }
+        },
+        browsers: ['ChromeHeadlessCI'], // Usa el navegador configurado
+        singleRun: true,
+        restartOnFileChange: false
     });
-  };
+};
